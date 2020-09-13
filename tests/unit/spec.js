0996c5cbeb24a4bea25fb04fc0bade3bf85b4c33
cpears93 <cpearson3140@gmail.com>
2020-09-13 13:36:21 +0100

    Nested tests

--- tests/unit/spec.js
+++ tests/unit/spec.js
@@ -9,42 +9,42 @@ describe ('signUp', function () {
 //Tests will be written in the form of "The user must..."
 
 user ('must be able to sign up as a new user', function () {
-signUp.submit("New User")    
-assert.strictEqual(true, signUp.new_user)
+    signUp.submit("New User")    
+    assert.strictEqual(true, signUp.new_user)
 });
 
 user ('must sign up using 2 factor authentication', function () {
-signUp.submit(7348)    
-assert.strictEqual(7348, signUp.two_factor)
+    signUp.submit(7348)    
+    assert.strictEqual(7348, signUp.two_factor)
 });
 
 user ('must verify their email address', function () {
-signUp.submit("Brian.Jennings@hotmail.com")    
-assert.strictEqual("Brian.jennings@hotmail.com", signUp.email_confirmation)
+    signUp.submit("Brian.Jennings@hotmail.com")    
+    assert.strictEqual("Brian.jennings@hotmail.com", signUp.email_confirmation)
 });
 
 user ('must enter a valid phone number', function () {
-signUp.submit(03277626745)    
-assert.strictEqual(03277626745, signUp.phone_number)
+    signUp.submit(03277626745)    
+    assert.strictEqual(03277626745, signUp.phone_number)
 });
 
 user ('must recieve a one-time password', function () {
-signUp.submit("K4i&s@")    
-assert.strictEqual("K4i&s@", signUp.otp_password)
+    signUp.submit("K4i&s@")    
+    assert.strictEqual("K4i&s@", signUp.otp_password)
 });
 
 user ('must create and confirm a 6 digit pin', function () {
-signUp.submit(614829)   
-assert.strictEqual(614829, signUp.otp_password)
+    signUp.submit(614829)   
+    assert.strictEqual(614829, signUp.otp_password)
 });
 
 user ('must confirm to the user a successful sign up', function () {
-signUp.submit("New User")  
-signUp.submit(7348)
-signUp.submit("Brian.Jennings@hotmail.com")
-signUp.submit(03277626745)
-signUp.submit("K4i&s@")
-signUp.submit(614829)
+    signUp.submit("New User")  
+    signUp.submit(7348)
+    signUp.submit("Brian.Jennings@hotmail.com")
+    signUp.submit(03277626745)
+    signUp.submit("K4i&s@")
+    signUp.submit(614829)
 assert.strictEqual("New User", 7348, "Brian.Jennings@hotmail.com", 03277626745, "K4i&s@", 614829, signUp.confirm_signup)
 });
 
